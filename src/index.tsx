a442774d58bd0ca1636655d6e06389c4ffdad45a
jennifer.cooper <jennifer.cooper@jisc.ac.uk>
2024-01-11 20:25:07 +0000

    feat: new page when clicking

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-    BrowserRouter, Routes, Route
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/HomePage/HomePage';
 import BookList from "./components/BookList/BookList";
@@ -10,12 +8,11 @@ import BookDetails from "./components/BookDetails/BookDetails";
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-        <BrowserRouter>
-            <Routes>
-                <Route path = "/" element = {<HomePage />}>
-                    <Route path = "book" element = {<BookList />} />
-                    <Route path = "/book/:id" element = {<BookDetails />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="book" element={<BookList />} />
+            <Route path="book/:id" element={<BookDetails />} />
+        </Routes>
+    </BrowserRouter>
 );
